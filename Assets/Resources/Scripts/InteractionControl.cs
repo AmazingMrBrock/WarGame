dfcde66764f118160bcd74b792265c6b4ed55ffe
BPC\B <dramascus@gmail.com>
2015-07-18 00:45:16 -0700

    End of evening

--- Assets/Resources/Scripts/InteractionControl.cs
+++ Assets/Resources/Scripts/InteractionControl.cs
@@ -6,8 +6,12 @@ public class InteractionControl : MonoBehaviour {
 	/// Controls interactions with the map and game pieces.
 	/// </summary>
 
-	public delegate void DSelected(GameObject tempObj);
+	public delegate void DSelect(GameObject tempObj);
+	public static DSelect DSelected;
 
+	int arrayIndex = 0;
+
+	GameObject[] unitAndDest = new GameObject[2];
 
 	void Awake () {
 		DSelected = OnSelect;
@@ -18,10 +22,18 @@ public class InteractionControl : MonoBehaviour {
 	}
 
 	void OnSelect(GameObject tempObj){
-
+		if(arrayIndex == 0){
+			unitAndDest[0] = tempObj;
+			arrayIndex++;
+		}
+		if(arrayIndex == 1){
+			unitAndDest[1] = tempObj;
+			arrayIndex = 0;
+			UnitMove(unitAndDest[0], unitAndDest[1]);
+		}
 	}
 
-	void UnitMove(){
+	void UnitMove(GameObject tempUnit, GameObject tempDest){
 
 	}
 }
