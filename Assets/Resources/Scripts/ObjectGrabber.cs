3ff427a8962e36b0808575cb2517fd472184ef8c
BPC\B <dramascus@gmail.com>
2015-07-17 20:43:08 -0700

    ObjectGrabberDesigned

---
+++ Assets/Resources/Scripts/ObjectGrabber.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectGrabber : MonoBehaviour {
+	/// <summary>
+	/// Creates a dictionary of units for each team and makes the dictionaries available for copy.
+	/// </summary>
+
+	Dictionary<string, UnitData> whiteUnitDic = new Dictionary<string, UnitData>();
+	Dictionary<string, UnitData> blackUnitDic = new Dictionary<string, UnitData>();
+	Dictionary<string, Vector3> spotVectDic = new Dictionary<string, Vector3>();
+
+	public int unitCache;
+	public int spotCache;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void SpotGrabber(){
+
+	}
+
+	void UnitGrabber(){
+		GameObject[] tempUnit = GameObject.FindGameObjectsWithTag("Unit");
+
+
+		for(int i = 0; i < tempUnit.Length; i++){
+
+			UnitData tempData;
+			string tempID = tempUnit[i].transform.name + i;
+			tempData.unitID = tempID;
+			Transform unitParent = tempUnit[i].GetComponentInParent<Transform>();
+			string tempSide = unitParent.transform.name;
+			tempData.unitSide = tempSide;
+			Vector3 tempVec = tempUnit[i].transform.position;
+			tempData.unitPos = tempVec;
+			if(tempSide == "White"){
+				whiteUnitDic.Add(tempID, tempData)
+			}
+			else if(tempSide == "Black"){
+				blackUnitDic.Add(tempID, tempData);
+			}
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/UnitData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class UnitData {
+
+	public string unitID;
+	public string unitSide;
+	public bool unitActive;
+	public Vector3 unitPos;
+
+
+	public UnitData(string ID, string side, bool active, Vector3 position){
+		this.unitID = ID;
+		this.unitSide = side;
+		this.unitActive = active;
+		this.unitPos = position;
+	}
+}
